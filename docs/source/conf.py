ce91386bb31a76dec1d6483969184a4647ac3ebb
TTheGlock <maxence.lechevalliercoussot@gmail.com>
2023-11-06 16:09:01 +0100

    Ajout de fichiers relatifs à Sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,13 @@ author = 'Maxence Lechevallier-Coussot, Enzo Cecotti, Colin Rousselot, Khad Char
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+'sphinx.ext.todo',
+'sphinx.ext.mathjax',
+'sphinx.ext.ifconfig',
+'sphinx.ext.autodoc',
+'sphinx.ext.viewcode',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
