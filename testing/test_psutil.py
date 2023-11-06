5f720b6e4c9ac0c9ebee3ca20c3cd8b32755f573
TTheGlock <maxence.lechevalliercoussot@gmail.com>
2023-11-06 15:08:57 +0100

    MAJ du fichier de récupération des données

--- testing/test_psutil.py
+++ testing/test_psutil.py
@@ -1,13 +1,12 @@
+import socket
 import platform
 import psutil
 import getmac
 import shutil
 
-total, used, free = shutil.disk_usage("/")
-
 #----- Système -----
 
-nom = platform.node()
+nom = platform.node()  #ex : PC de Jacques
 architecure = platform.machine()  #ex : x86_64
 plateforme = platform.platform()  #ex : Linux-6.5.8-200.fc38.x86_64-x86_64-with-glibc2.37
 systeme = platform.system()  #ex : Linux
@@ -21,6 +20,11 @@ cpu_charge = str(psutil.cpu_percent(interval=cpu_intervalles))
 
 #----- Disque -----
 
+if systeme == "Linux":
+    total, used, free = shutil.disk_usage("/")
+elif systeme == "Windows":
+    total, used, free = shutil.disk_usage("C:\\")    
+
 total_gb = total // (2**30)  #transformation de bits en gigabits
 used_gb = used // (2**30)  #//
 free_gb = free // (2**30)  #//
@@ -29,3 +33,4 @@ en_tout = used_gb/total_gb*100
 #----- Réseau -----
 
 adresse_mac = str(getmac.get_mac_address())
+adresse_ip = socket.gethostbyname(socket.gethostname()) #ipv4 locale
