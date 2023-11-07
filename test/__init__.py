2367dd24c85c6c7399f5847ca15b80ce6d36728b
TTheGlock <maxence.lechevalliercoussot@gmail.com>
2023-11-07 10:15:11 +0100

    MAJ de l'arborescence (2)

---
+++ test/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '0.1.0'
+
+""" Mon projet.
+
+.. moduleauthor:: LECHEVALLIER-COUSSOT Maxence <maxence.lechevallier.coussot@etu.univ-poitiers.fr>
+                  CECOTTI Enzo <enzo.cecotti@etu.univ-poitiers.fr>
+                  ROUSSELOT Colin <colin.rousselot@etu.univ-poitiers.fr>
+                  HOUNGBO Charbel <khad.houngbo@etu.univ-poitiers.fr>
+                  BA Abdourahmane <abdourahmane.ba@etu.univ-poitiers.fr>
+
+"""
