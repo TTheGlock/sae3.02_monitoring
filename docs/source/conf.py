d5e0c9eb6442a29b7544b8fa12c7ca9f9a5d34cf
TTheGlock <maxence.lechevalliercoussot@gmail.com>
2023-11-07 09:17:18 +0100

    MAJ de la documentation Sphinx (2)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,13 @@ author = 'LECHEVALLIER-COUSSOT Maxence, CECOTTI Enzo, ROUSSELOT Colin, HOUNGBO C
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+'sphinx.ext.todo',
+'sphinx.ext.mathjax',
+'sphinx.ext.ifconfig',
+'sphinx.ext.autodoc',
+'sphinx.ext.viewcode',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
