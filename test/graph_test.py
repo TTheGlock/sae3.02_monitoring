47f604296e9c4535824ae02ff799db2385958b86
ececotti <encecot@hotmail.com>
2023-12-21 20:45:06 +0100

    dashboard tableau avec graph test

--- test/graph_test.py
+++ test/graph_test.py
@@ -1,35 +1,40 @@
-import matplotlib.pyplot as plt
+# Importez la bibliothèque graphique Qt
+import sys
+from PySide6.QtCharts import QChart, QBarSeries, QBarSet, QChartView, QLineSeries
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGridLayout, QTableWidget
 
+# Créez une application Qt
+app = QApplication(sys.argv)
+
+# Créez une fonction pour créer un graphique
 def graph(data):
-    # Créer le graphique
-    plt.plot(data, color='blue', linestyle='solid')
+    # Créez un graphique
+    chart = QChart()
 
-    # Configurer le graphique
-    plt.xlabel("Temps (s)")
-    plt.ylabel("Charge CPU (%)")
-    plt.ylim([0, 100])
-    plt.xticks([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    # Ajoutez les données au graphique
+    for i in range(len(data)):
+        chart.addSeries(QLineSeries([(i, data[i])]).setName(f"Point {i}"))
 
-    # Ajouter un titre
-    plt.title("Charge CPU")
+    # Configurez le graphique
+    chart.setTitle("Charge CPU")
+    chart.setYRange(0, 100)
+    chart.setXRange(0, 9)
 
-    # Ajouter une légende
-    plt.legend(["Charge CPU"], loc="upper left")
-
-    # Ajouter des bordures
-    plt.grid(axis='both', linestyle='--')
-
-    # Aligner les données
-    plt.xticks([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    # Créez un widget QChartView
+    chartView = QChartView(chart)
 
     # Afficher le graphique
-    plt.show()
+    chartView.show()
 
+    # Afficher un message
+    print("Le graphique a été créé avec succès.")
 
+# Définir les données
+data = [35, 36, 37, 38, 39, 38, 37, 53, 53, 54]
+
+# Afficher le graphique
 if __name__ == "__main__":
-    # Définir les données
-    data = [35, 36, 37, 38, 39, 38, 37, 53, 53, 54]
-
-    # Afficher le graphique
     graph(data)
 
+# Exécutez l'application
+app.exec_()
