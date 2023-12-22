a6ea319a012501d4063ed072b09ca1f1a792d85f
Khadc <charbelhoungbo@gmail.com>
2023-12-22 13:22:29 +0100

    Commentaires graph

--- sae3.02_monitoring/graph.py
+++ sae3.02_monitoring/graph.py
@@ -1,42 +1,43 @@
-import sys
-import pyqtgraph as pg
+import sys  # Importation du module sys pour les opérations système.
+import pyqtgraph as pg  # Importation de la bibliothèque pyqtgraph sous l'alias pg.
 
 def graph_1(data, nom_charge):
-    # create plot
+    # Création d'un objet de type plot pour afficher le graphique.
     plt = pg.plot()
-    plt.showGrid(x=True, y=True)
-    plt.addLegend()
+    plt.showGrid(x=True, y=True)  # Affichage des grilles pour les axes x et y.
+    plt.addLegend()  # Ajout d'une légende au graphique.
 
-    # set properties
-    plt.setLabel('left', 'Taux d utilisage', units='%')
-    plt.setLabel('bottom', 'Temps', units='s')
-    plt.setXRange(0, 10)
-    plt.setYRange(0, 100)
-    plt.setWindowTitle(nom_charge)
+    # Configuration des propriétés du graphique.
+    plt.setLabel('left', 'Taux d utilisage', units='%')  # Label de l'axe des ordonnées.
+    plt.setLabel('bottom', 'Temps', units='s')  # Label de l'axe des abscisses.
+    plt.setXRange(0, 10)  # Définition de la plage des valeurs de l'axe des abscisses.
+    plt.setYRange(0, 100)  # Définition de la plage des valeurs de l'axe des ordonnées.
+    plt.setWindowTitle(nom_charge)  # Attribution du titre de la fenêtre du graphique.
 
-    # plot
+    # Traçage du graphique.
     plt.plot(range(0, 10), data, pen='b', symbol='x', symbolPen='b', symbolBrush=0.2, name='red')
+    # Traçage des données avec des points reliés par des lignes bleues et des croix en tant que symboles.
 
-    return plt
+    return plt  # Renvoi de l'objet graphique créé.
 
 
 def main():
-    app = pg.QtWidgets.QApplication(sys.argv)
+    app = pg.QtWidgets.QApplication(sys.argv)  # Création d'une instance de l'application PyQtGraph.
 
-    # Get data from another program
+    # Obtention des données à partir d'un autre programme (données factices).
     data = [10, 20, 30, 40, 50, 10, 20, 30, 40, 50]
 
-    nom = 'bonsoir'
+    nom = 'bonsoir'  # Attribution d'un nom (factice) à utiliser comme titre pour le graphique.
 
-    # Call the graph function
-    graph = graph_1(data,nom)
+    # Appel de la fonction graph_1 avec les données et le nom en paramètres.
+    graph = graph_1(data, nom)
 
-    # Display the graph
+    # Affichage du graphique.
     graph.show()
 
-    # Terminate the application
+    # Exécution de l'application et gestion de l'événement d'arrêt.
     sys.exit(app.exec())
 
 
 if __name__ == "__main__":
-  main()
+    main()  # Appel de la fonction main lorsque le fichier est exécuté en tant que script.
